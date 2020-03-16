0ac0663c3795155dea57515b1e683477fece2684
nik <nirvikchakraborty02@gmail.com>
2020-03-16 20:26:55 +0530

    Upto Recipe Model addition and displaying hardcoded data

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+recipes:Recipe[]=[
+  new Recipe('A Test Recipe','This is simply a test','https://www.budgetbytes.com/wp-content/uploads/2013/07/Creamy-Tomato-and-Spinach-Pasta-skillet-1-480x270.jpg')
 
+]; //array of recipes of Type Recipe
   constructor() { }
 
   ngOnInit(): void {
